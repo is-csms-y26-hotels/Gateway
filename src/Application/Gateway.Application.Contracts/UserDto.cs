7a3ddcecbf82401d0a9d1b2a375859f746ac8060
theSirD <isaev-daniel17@yandex.ru>
2025-01-25 16:32:21 +0300

    refactor: changes modesl

--- src/Application/Gateway.Application.Contracts/UserDto.cs
+++ src/Application/Gateway.Application.Contracts/UserDto.cs
@@ -3,35 +3,36 @@ using System.Text.Json.Serialization;
 namespace Gateway.Application.Contracts;
 
 public record UserDto(
-    long? UserId,
-    string FirstName,
-    string LastName,
-    string Email,
-    string Password,
-    DateTime Birthdate,
-    Sex Sex,
-    string? Tel,
-    DateTime CreatedAt)
+    [property: JsonPropertyName("userId")] long? UserId,
+    [property: JsonPropertyName("firstName")] string FirstName,
+    [property: JsonPropertyName("lastName")] string LastName,
+    [property: JsonPropertyName("email")] string Email,
+    [property: JsonPropertyName("password")] string Password,
+    [property: JsonPropertyName("birthdate")] DateTime Birthdate,
+    [property: JsonPropertyName("sex")] Sex Sex,
+    [property: JsonPropertyName("tel")] string? Tel,
+    [property: JsonPropertyName("createdAt")] DateTime CreatedAt)
 {
-    [JsonConstructor]
-    public UserDto(
-        string firstName,
-        string lastName,
-        string email,
-        string password,
-        DateTime birthdate,
-        Sex sex,
-        DateTime createdAt)
-        : this(
-            null,
-            firstName,
-            lastName,
-            email,
-            password,
-            birthdate,
-            sex,
-            null,
-            createdAt)
-    {
-    }
+    // [JsonConstructor]
+    // public UserDto(
+    //     long userId,
+    //     string firstName,
+    //     string lastName,
+    //     string email,
+    //     string password,
+    //     DateTime birthdate,
+    //     Sex sex,
+    //     DateTime createdAt)
+    //     : this(
+    //         null,
+    //         firstName,
+    //         lastName,
+    //         email,
+    //         password,
+    //         birthdate,
+    //         sex,
+    //         null,
+    //         createdAt)
+    // {
+    // }
 }
